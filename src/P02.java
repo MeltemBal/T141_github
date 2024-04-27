550482fb26e56b0bb60fc76cf1e4b2599aca1ec4
melbal <mbal@gmail.com>
2024-04-27 21:44:32 +0300

    second version

---
+++ src/P02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P02 {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Adınızı giriniz");
+        String isim= scan.nextLine();
+
+    }
+}
